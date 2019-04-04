a3f3a982f85ec741f3853b8ef6557dbccf58dc6b
drj0e <49249713+drj0e@users.noreply.github.com>
2019-04-04 14:33:54 -0400

    Add files via upload

---
+++ DRIVE_LOGIC2.ps1
@@ -0,0 +1,27 @@
+﻿ $mypath0 = ("C:\Program Files (x86)\Agilent Technologies\ECM Application Service\logs")
+ $mypath1 = ("D:\Program Files (x86)\Agilent Technologies\ECM Application Service\logs")
+ $mypath2 = ("E:\Program Files (x86)\Agilent Technologies\ECM Application Service\logs")
+ 
+ $a = Test-Path $mypath0, $mypath1, $mypath2
+
+    if ($a[0] -eq $True) {
+    Write-Host "HEY IM HERE!!!!! :)"
+    Write-Host $mypath0
+    Copy-Item $mypath0  -Destination "C:\Temp\Tester1" -Recurse 
+ }
+    else { Write-Host "Not here"
+ }
+      if ($a[1] -eq $True) {
+    Write-Host "HEY IM HERE!!!!! :)"
+    Write-Host $mypath1
+    Copy-Item $mypath1  -Destination "C:\Temp\Tester1" -Recurse 
+ }
+    else { Write-Host "Not here"
+ }
+    if ($a[2] -eq $True) {
+    Write-Host "HEY IM HERE!!!!! :)"
+    Write-Host $mypath2
+    Copy-Item $mypath2  -Destination "C:\Temp\Tester1" -Recurse 
+ }
+    else { Write-Host "Not here"
+ }
---
+++ ECM_Logs00.ps1
@@ -0,0 +1,21 @@
+﻿#Script to copy logs files and stuff#
+#Create directory for logs, add folder for ISS Logs from IISLOGS.ps1#
+New-Item -Path "C:\" -Name "Temp2" -ItemType "directory"
+New-Item -Path "C:\Temp2" -Name "IISLOGS" -ItemType "directory"
+
+#Copy Logs files from soruce to destination#
+
+Copy-Item "C:\Windows\System32\winevt\Logs\System.evtx" -Destination "C:\Temp2"
+Copy-Item "C:\Windows\System32\winevt\Logs\Application.evtx" -Destination "C:\Temp2"
+Copy-Item "C:\Windows\System32\winevt\Logs\Security.evtx" -Destination "C:\Temp2"
+Copy-Item "C:\Program Files (x86)\Agilent Technologies\ECM Application Service\logs" -Destination "C:\Temp2\AppLogs" -Recurse
+Copy-Item "C:\Program Files (x86)\Agilent Technologies\IQTool\SFVTool\IQProducts\*" -Destination "C:\Temp2\IQPRODUCTS" -Recurse
+Copy-Item "C:\SVReports\*" -Destination "C:\Temp2\SVRREPORTS" -Recurse
+Copy-Item "C:\Users\opl_svc\AppData\Roaming\Agilent Technologies\ECM Server Configuration Wizard\1.0.0.0" -Destination "C:\Temp2\CONFIGWIZARD" -Recurse
+Copy-Item "C:\Users\opl_svc\AppData\Roaming\Agilent Technologies\HotFixes" -Destination "C:\Temp2\HOTFIXES" -Recurse
+
+#Run IISLOGS to collect the IIS log files for 10 days#
+& C:\Temp\IISLOGS.ps1
+
+#Zip and copy the final result set to the user desktop#
+Compress-Archive -LiteralPath C:\Temp2 -CompressionLevel Optimal -DestinationPath "%userprofile%\Desktop\ECM Logs" -Force
